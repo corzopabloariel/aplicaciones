d5d3d1acf3215a49fa4911e627dbf4a2e3fdd690
Pablo Corzo <corzo.pabloariel@gmail.com>
2020-09-15 21:34:10 -0300

    Func que lee json es async. FIX

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-    $(".grid-element").click(function(event) {
-        event.preventDefault();
-        let valor = 0;
-        $(this).toggleClass("grid-click");
-        $(".grid-click").each(function() {
-            valor += parseFloat($(this).data("precio"));
-        });
-        valor = valor.toFixed(2);
-        valor = valor.replace(".", ",");
-        $(".carrito").text(`$ ${valor}`);
-        return false;
+function carritoLink(event) {
+    event.preventDefault();
+    let valor = 0;
+    $(this).toggleClass("grid-click");
+    $(".grid-click").each(function() {
+        valor += parseFloat($(this).data("precio"));
     });
+    valor = valor.toFixed(2);
+    valor = valor.replace(".", ",");
+    $(".carrito").text(`$ ${valor}`);
+    return false;
+}
+$(document).ready(function() {
+    $(".grid-element").click(carritoLink);
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -36,6 +36,8 @@ function products(...args) {
         a.classList.add("grid-element");
         a.dataset.precio = elem.price === undefined ? 50 : elem.price;
         a.href = window.flagCarrito === undefined ? `pelicula.html#${args[1]}-${elem.code}` : "#";
+        if (window.flagCarrito !== undefined)
+            a.setAttribute("onclick", "carritoLink(event)");
         html += `<picture>`;
             html += `<img src="${img}"  class="w-100" onError="this.src='https://www.unaj.edu.ar/wp-content/uploads/2016/06/logo-unaj-2016-01.jpg'" alt="${elem.title}">`;
         html += `</picture>`;
