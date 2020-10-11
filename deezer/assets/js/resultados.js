52881a02c717c9380eed7c5897f0a844c5329533
Pablo <pablovzqz768@gmail.com>
2020-10-11 01:21:11 -0300

    arreglo el merge del index que hice en el anterior commit pro accidente, vuelvo a lo ultimo de pablo corzo

--- deezer/assets/js/resultados.js
+++ deezer/assets/js/resultados.js
@@ -5,9 +5,10 @@ busqueda=document.getElementById('q').value;
 console.log(busqueda);
 
 
-url=`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}&index=1&limit=10?output=json`;
+url=`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}&index=0&limit=10?output=json`;
 $.get(url, function (result) {
     console.log(url);
+    index=0;
     index=result.total;
     parseInt(index);
    // result=JSON.parse(result);
@@ -16,15 +17,14 @@ $.get(url, function (result) {
     console.log(index);
     var segundos=parseInt(obj.duration);
     
-        $("#data").append(`
+        $("#datos").append(`
     
-    <div class="music">
+
     <div id='${obj.id}' class="music">
     <picture>
     <img src='${obj.album.cover_medium}'>
     </picture>
     <div class="text">
-        <a href="">Título 1</a>
         <a href="">${obj.title}</a>
         <ul class="details">
            
@@ -32,7 +32,17 @@ $.get(url, function (result) {
             <li>${aMinutos(segundos)} </li>
         </ul>
     </div>
-</div>`);})})}
+</div>`
+)
+/*if(result.hasOwnProperty('next')){
+    var total=result.total;
+    total=total/10;
+    while (index<total){
+
+    }
+}*/
+
+})})}
 
 function aMinutos(time) {
     var hr = ~~(time / 3600);
