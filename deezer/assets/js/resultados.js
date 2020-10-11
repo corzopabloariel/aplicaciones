eef00f0f3b2f465e214871a4d4794f8b91f4c361
Pablo Corzo <corzo.pabloariel@gmail.com>
2020-10-11 20:07:52 -0300

    Cambios en style2.css y resultados.js

--- deezer/assets/js/resultados.js
+++ deezer/assets/js/resultados.js
@@ -1,59 +1,50 @@
 var i;
 function resultados () {
     // Validar contenido de #q
-console.log(document.getElementById('q').value);
-busqueda=parametro.get('q');
-i=parametro.get('index');
-console.log(busqueda);
-/**
- * if busqueda is not Empty -> document.getElementById('q').value = busqueda;
- */
-    
-    url=`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}&index=${i}&limit=10?output=json`;
+    console.log(document.getElementById('q').value);
+    busqueda = parametro.get('q').trim();
+    if (busqueda === "")
+        return;
+    document.getElementById('q').value = busqueda;
+    i = parametro.get('index');
+    console.log(busqueda);
+    /**
+     * if busqueda is not Empty -> document.getElementById('q').value = busqueda;
+     */
+    url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}&index=${i}&limit=10?output=json`;
     console.log(url);
-$.get(url, function (result) {
-    console.log(result);
-    i=0;
-   // result=JSON.parse(result);
+    $.get(url, function (result) {
+        console.log(result);
+        i = 0;
+    // result=JSON.parse(result);
 
-  $.each(result.data,function(index,obj){
-    i=index++;
-    console.log(index);
+        $.each(result.data, function(index,obj){
+            i = index ++;
+            console.log(index);
+            console.log(i);
+            var segundos = parseInt(obj.duration);
+            $("#datos_encontrados").append(
+                `<div id='${obj.id}' class="music">` +
+                    `<picture>` +
+                        `<img class="music--cover" src="${obj.album.cover_medium}">` +
+                    `</picture>` +
+                    `<div class="text">` +
+                        `<div class="music--title"><a href="">${obj.title}</a></div>` +
+                        `<ul class="music--details">` +
+                            `<li id='artist-"${obj.artist.id}"' data-type="artist">${obj.artist.name}</li>` +
+                            `<li data-type="time">${aMinutos(segundos)}</li>` +
+                        `</ul>` +
+                    `</div>` +
+                `</div>`
+            );
+        })
 
-    console.log(i);
-    var segundos=parseInt(obj.duration);
-    
-        $("#datos_encontrados").append(`
-    
-
-    <div id='${obj.id}' class="music">
-    <picture>
-    <img src='${obj.album.cover_medium}'>
-    </picture>
-    <div class="text">
-        <a href="">${obj.title}</a>
-        <ul class="details">
-           
-            <li id='"${obj.artist.id}"'>${obj.artist.name}</li>
-            <li>${aMinutos(segundos)} </li>
-        </ul>
-    </div>
-</div>`
-)
-
-})
-
-/**
- * la clase de css d-none, tiene para ocultar
- * cada vez que se haga una nueva búsqueda debería ocultarse
- */
-
-if(result.hasOwnProperty('next')||i==9){
-index=new URLSearchParams(result.next);
-index=index.get("index")
-$(".paginate").removeClass("d-none");
-$(".paginate--link").attr("href",`index.html?q=${busqueda}&index=${index}`);}
-})
+    if(result.hasOwnProperty('next') || i == 9) {
+        index = new URLSearchParams(result.next);
+        index = index.get("index")
+        $(".paginate").removeClass("d-none");
+        $(".paginate--link").attr("href",`index.html?q=${busqueda}&index=${index}`);}
+    })
 }
 
 function aMinutos(time) {
@@ -61,16 +52,14 @@ function aMinutos(time) {
     var min = ~~((time % 3600) / 60);
     var sec = time % 60;
     var sec_min = "";
-    if (hr > 0) {
-       sec_min += "" + hrs + ":" + (min < 10 ? "0" : "");
-    }
-    sec_min += "" + min + ":" + (sec < 10 ? "0" : "");
-    sec_min += "" + sec;
+    if (hr > 0)
+        sec_min = `${hrs}:`;
+    sec_min += `${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`;
     return sec_min+ " min";
- }
+}
 
- function genero(){
-     id=parametro.get("categoria")
+function genero(){
+    id = parametro.get("categoria")
     url=`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists/?index=0&limit=10?output=json`;
     $.get(url, function (result) {
         console.log(url);
@@ -79,24 +68,21 @@ function aMinutos(time) {
         parseInt(index);
        // result=JSON.parse(result);
         console.log(result.data);
-      $.each(result.data,function(index,obj){
-        console.log(index);
-        var segundos=parseInt(obj.duration);
-        
-            $("#datos_encontrados").append(`
-        
-    
-        <div id='${obj.id}' class="music">
-        <picture>
-        <img src='${obj.picture_medium}'>
-        </picture>
-        <div class="text">
-            <a href="">${obj.name}</a>
-            <ul class="details">
-  
-            </ul>
-        </div>
-    </div>`
-    )
- })})
+        $.each(result.data,function(index,obj){
+            console.log(index);
+            var segundos=parseInt(obj.duration);
+            $("#datos_encontrados").append(
+                `<div id='${obj.id}' class="music">` +
+                    `<picture>` +
+                        `<img src='${obj.picture_medium}'>` +
+                    `</picture>` +
+                    `<div class="text">` +
+                        `<a href="">${obj.name}</a>` +
+                        `<ul class="details">` +
+                        `</ul>` +
+                    `</div>` +
+                `</div>`
+            );
+        })
+    })
 }
